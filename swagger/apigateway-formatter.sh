182a2fddfb3a34a2dde26415d823617d961fb53c
Henrique <hlisboasousa@gmail.com>
2023-11-28 15:11:38 -0300

    finishing bash script

--- swagger/apigateway-formatter.sh
+++ swagger/apigateway-formatter.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-# Check if the env argument is provided
+
+# This script does the following:
+#   - formats the OpenAPI file to be compatible with API Gateway. 
+#   - updates and deploys the API Gateway to the environment provided as argument.
+#   - updates the Swagger UI docs in S3.
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <env>"
     exit 1
 fi
 
 ENV="$1"
-# Read the OpenAPI file into a variable
-openapi_file="openapi.json"
-base_uri="https://\${stageVariables.SERVICE_NAME}.$ENV.llzgarantidora.com"
+SERVICE_NAME="administradora"
+BASE_URI="https://${SERVICE_NAME}.$ENV.llzgarantidora.com"
+OPENAPI_JSON_PATH="openapi.json"
+
+BUCKET_NAME="dev-api-swagger"
+DOCS_DIR="$BUCKET_NAME/docs"
+DOCS_LIST_JSON="$BUCKET_NAME/docs-list.json"
+
 
 # Function to process each path in the OpenAPI file
 process_path() {
@@ -26,7 +36,7 @@ process_path() {
 {
   "x-amazon-apigateway-integration": {
     "httpMethod": "$method",
-    "uri": "$base_uri$path",
+    "uri": "$BASE_URI$path",
     "responses": {
       "default": {
         "statusCode": "200"
@@ -53,10 +63,10 @@ EOL
 }
 
 # Extract paths from the OpenAPI file
-paths=$(jq -c '.paths | to_entries[]' "$openapi_file")
+paths=$(jq -c '.paths | to_entries[]' "$OPENAPI_JSON_PATH")
 
 # Initialize openapi_data
-openapi_data=$(cat "$openapi_file")
+openapi_data=$(cat "$OPENAPI_JSON_PATH")
 
 # Loop through each path and process it
 while IFS= read -r path_entry; do
@@ -86,11 +96,57 @@ fi
 # Check if openapi_data is not empty before overwriting the file
 if [ -n "$openapi_data" ]; then
   # Overwrite the original OpenAPI file
-  echo "$openapi_data" > "$openapi_file"
-  echo "Script executed successfully."
+  echo "$openapi_data" > "$OPENAPI_JSON_PATH"
+  echo "openapi.json formatted succesfully."
 else
-  echo "Error: openapi_data is empty. Script did not execute successfully."
+  echo "Error: openapi.json is empty. Script did not execute successfully."
+  exit 1
 fi
 
-aws apigateway put-rest-api --rest-api-id "enq3zuko73" --mode overwrite --body "fileb://openapi.json"
-aws apigateway create-deployment --rest-api-id "enq3zuko73" --stage-name "$ENV"
+aws apigateway put-rest-api --rest-api-id "enq3zuko73" --mode overwrite --body "fileb://openapi.json" > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update the API."
+    exit 1
+else
+    echo "API updated successfully."
+fi
+
+aws apigateway create-deployment --rest-api-id "enq3zuko73" --stage-name "$ENV" > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to deploy the API."
+    exit 1
+else
+    echo "API deployed successfully."
+fi
+
+# Update Swagger-UI on S3
+
+aws s3 cp "$OPENAPI_JSON_PATH" "s3://$DOCS_DIR/$SERVICE_NAME.json"
+
+# Download the current docs-list.json file from S3
+aws s3 cp "s3://$DOCS_LIST_JSON" "current-docs-list.json"
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Swagger UI update - downloading current docs list."
+    exit 1
+fi
+
+# Add the new entry to the docs list
+new_entry="{\"url\": \"docs/$SERVICE_NAME.json\", \"name\": \"$SERVICE_NAME\"}"
+jq ". += [$new_entry]" current-docs-list.json > updated-docs-list.json
+
+# Upload the updated docs-list.json file back to S3
+aws s3 cp "updated-docs-list.json" "s3://$DOCS_LIST_JSON"
+
+# Check if the upload was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Swagger UI update - error on uploading docs list."
+    exit 1
+fi
+
+# Clean up temporary files
+rm current-docs-list.json updated-docs-list.json
+echo "Swagger UI updated successfully."
